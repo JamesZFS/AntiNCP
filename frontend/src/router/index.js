7cb3938356cef41a2b589875272af18d65a6e1c4
wzx0645 <zixuan-w17@mails.tsinghua.edu.cn>
2020-03-02 01:18:01 +0800

    [init]frontend components files&routers

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import HomePage from '@/components/HomePage'
+import Page1 from '@/components/Page1'
+import Page2 from '@/components/Page2'
+import Page3 from '@/components/Page3'
+import Page4 from '@/components/Page4'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'HomePage',
+      component: HomePage
+    },
+    {
+      path: '/1',
+      name: 'Page1',
+      component: Page1
+    },
+    {
+      path: '/2',
+      name: 'Page2',
+      component: Page2
+    },
+    {
+      path: '/3',
+      name: 'Page3',
+      component: Page3
+    },
+    {
+      path: '/4',
+      name: 'Page4',
+      component: Page4
     }
   ]
 })
