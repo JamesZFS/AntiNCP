a7ad18c3a4cb5062a3fcc9429c39e710a1bd9baa
郑逢时 <962907278@qq.com>
2020-03-07 23:23:33 +0800

    [update] a naive unified backend demo, still in progress #1

--- backend/config/apis.js
+++ backend/config/apis.js
@@ -1,4 +1,10 @@
 const AMINER_EPIDEMIC_API = 'https://innovaapi.aminer.cn/predictor/api/v1/valhalla/hotevents/pneumonia/dxy';
 const AMINER_EPIDEMIC_API_UPDATE_INTERVAL = 12 * 3600 * 1000; // 12 hours
 
-module.exports = {AMINER_EPIDEMIC_API, AMINER_EPIDEMIC_API_UPDATE_INTERVAL};
+const DXY_AREA_API = 'https://raw.githubusercontent.com/BlankerL/DXY-COVID-19-Data/master/csv/DXYArea.csv'; // Ding XiangYuan
+const DXY_AREA_API_UPDATE_INTERVAL = 12 * 3600 * 1000; // 12 hours
+
+module.exports = {
+    AMINER_EPIDEMIC_API, AMINER_EPIDEMIC_API_UPDATE_INTERVAL,
+    DXY_AREA_API, DXY_AREA_API_UPDATE_INTERVAL
+};
--- backend/routes/retireve.js
+++ backend/routes/retireve.js
@@ -5,178 +5,122 @@ var axios = require('axios');
 var apis = require('../config/apis');
 var debug = require('debug')('backend:retrieve');
 var fs = require('fs');
-const cacheFilePath = 'public/data/epidemic.aminer.json';
-var epidemicData; // cached data
-// init epidemic data
-try {
-    epidemicData = JSON.parse(fs.readFileSync(cacheFilePath));
-} catch (err) {
-    console.error("Can't read from cache file.", err);
-    acquireEpidemicData().then(val => {
-        epidemicData = val;
-        fs.writeFileSync(cacheFilePath, JSON.stringify(epidemicData));
-        debug("Epidemic data updated.");
-    }).catch(err => {
-        console.error("Fatal: fail to update from epidemic data source.");
-        process.exit(1);
-    });
-}
+// const cacheFilePath = 'public/data/epidemic.aminer.json';
+const cacheFilePath = 'public/data/demo-response.json';
+var demoData = JSON.parse(fs.readFileSync(cacheFilePath)); // cached data
+var epidemicData;
+// // todo init epidemic data, #1
+// try {
+//     epidemicData = JSON.parse(fs.readFileSync(cacheFilePath));
+// } catch (err) {
+//     console.error("Can't read from cache file.", err);
+//     acquireEpidemicData().then(val => {
+//         epidemicData = val;
+//         fs.writeFileSync(cacheFilePath, JSON.stringify(epidemicData));
+//         debug("Epidemic data updated.");
+//     }).catch(err => {
+//         console.error("Fatal: fail to update from epidemic data source.");
+//         process.exit(1);
+//     });
+// }
+//
+// // todo
+// function acquireEpidemicData() { // async
+//     debug(`Acquiring epidemic data from source ${apis.AMINER_EPIDEMIC_API}`);
+//     return new Promise(function (resolve, reject) {
+//         axios.get(apis.AMINER_EPIDEMIC_API)
+//             .then(response => {
+//                 response.data.updateTime = Date.now();
+//                 resolve(response.data);
+//             })
+//             .catch(error => {
+//                 console.error("Can't access AMINER_EPIDEMIC_API.", error);
+//                 reject(error);
+//             });
+//     });
+// }
 
-function acquireEpidemicData() { // async
-    debug(`Acquiring epidemic data from source ${apis.AMINER_EPIDEMIC_API}`);
-    return new Promise(function (resolve, reject) {
-        axios.get(apis.AMINER_EPIDEMIC_API)
-            .then(response => {
-                response.data.updateTime = Date.now();
-                resolve(response.data);
-            })
-            .catch(error => {
-                console.error("Can't access AMINER_EPIDEMIC_API.", error);
-                reject(error);
-            });
-    });
-}
-
-// Retrieve middleware
-router.use('/epidemic', function (req, res, next) {
-    if (Date.now() - epidemicData.updateTime >= apis.AMINER_EPIDEMIC_API_UPDATE_INTERVAL) {
-        // periodically update epidemic data from source
-        acquireEpidemicData().then(val => {
-            epidemicData = val;
-            res.attached = epidemicData.data;
-            next();
-            fs.writeFile(cacheFilePath, JSON.stringify(epidemicData), err => { // async
-                if (err) console.error("Can't write back to cache file path.", err);
-                else debug("Epidemic data updated.");
-            });
-        }).catch(err => {
-            // fail to update
-            debug("Warning: fail to update from epidemic data source.");
-            res.attached = epidemicData.data; // use cached data
-            next();
-        })
-    } else {
-        // use cached data
-        res.attached = epidemicData.data;
-        next();
-    }
-});
+// // Retrieve middleware
+// router.use('/epidemic', function (req, res, next) {
+//     if (Date.now() - epidemicData.updateTime >= apis.AMINER_EPIDEMIC_API_UPDATE_INTERVAL) {
+//         // periodically update epidemic data from source
+//         acquireEpidemicData().then(val => {
+//             epidemicData = val;
+//             res.attached = epidemicData.data;
+//             next();
+//             fs.writeFile(cacheFilePath, JSON.stringify(epidemicData), err => { // async
+//                 if (err) console.error("Can't write back to cache file path.", err);
+//                 else debug("Epidemic data updated.");
+//             });
+//         }).catch(err => {
+//             // fail to update
+//             debug("Warning: fail to update from epidemic data source.");
+//             res.attached = epidemicData.data; // use cached data
+//             next();
+//         })
+//     } else {
+//         // use cached data
+//         res.attached = epidemicData.data;
+//         next();
+//     }
+// });
 
 /**
- * @api {get} /retrieve/epidemic/China  Get epidemic China api
- * @apiVersion 0.1.0
- * @apiName GetEpidemicChina
+ * @api {get} /retrieve/epidemic  Get epidemic data api
+ * @apiName GetEpidemicData
  * @apiGroup Retrieve
  * @apiPermission everyone
  *
- * @apiDescription Retrieve overall epidemic data in China
+ * @apiDescription Retrieve epidemic data in a specific superior place(world/country/province)
+ * !! this apis is in progress
  *
- * @apiSuccess {Object}  . json epidemic data
+ * @apiParam (Query Param) {string}  superiorPlace  superior place name
+ * @apiParam (Query Param) {string}  dataKind       epidemic data kind
  *
- * @apiExample Response (example):
- *  HTTP/1.1 200 OK
- *     {
- *         "currentConfirmedCount": 27524,
- *         "confirmedCount": 80422,
- *         "suspectedCount": 520,
- *         "curedCount": 49914,
- *         "deadCount": 2984,
- *         "seriousCount": 6416,
- *         "suspectedIncr": 143,
- *         "currentConfirmedIncr": -2572,
- *         "confirmedIncr": 120,
- *         "curedIncr": 2654,
- *         "deadIncr": 38,
- *         "seriousIncr": -390
- *     }
- */
-router.get('/epidemic/China', function (req, res) {
-    let data = res.attached.overall;
-    if (data === undefined) {
-        res.status(404).end();
-        return
-    }
-    res.status(200).send({
-        currentConfirmedCount: data.currentConfirmedCount,
-        confirmedCount: data.confirmedCount,
-        suspectedCount: data.suspectedCount,
-        curedCount: data.curedCount,
-        deadCount: data.deadCount,
-        seriousCount: data.seriousCount,
-        suspectedIncr: data.suspectedIncr,
-        currentConfirmedIncr: data.currentConfirmedIncr,
-        confirmedIncr: data.confirmedIncr,
-        curedIncr: data.curedIncr,
-        deadIncr: data.deadIncr,
-        seriousIncr: data.seriousIncr,
-    });
-});
-
-/**
- * @api {get} /retrieve/epidemic/province  Get epidemic province api
- * @apiVersion 0.1.0
- * @apiName GetEpidemicProvince
- * @apiGroup Retrieve
- * @apiPermission everyone
- *
- * @apiDescription Retrieve epidemic data in a specific province
- *
- * @apiParam (Query Param) {string}  q  province name
- *
- * @apiSuccess {Object}  . array of epidemic data in different cities
+ * @apiSuccess {Object}  . epidemic data in different inferior places and time stamps
  *
  * @apiExample {curl} Example usage:
- *     curl -i http://localhost:3000/retrieve/epidemic/province?p=四川省
+ *     curl "http://localhost:3000/retrieve/epidemic?superiorPlace=四川省&dataKind=confirmedCount"
  *
  * @apiExample Response (example):
- *  HTTP/1.1 200 OK
- *     [
- *         {
- *             "cityName": "成都",
- *             "currentConfirmedCount": 47,
- *             "confirmedCount": 143,
- *             "suspectedCount": 0,
- *             "curedCount": 93,
- *             "deadCount": 3,
- *             "locationId": 510100,
- *             "confirmedAddCount": 0,
- *             "suspectedAddCount": 0,
- *             "curedAddCount": 1,
- *             "deadAddCount": 0
- *         },
- *         {
- *             "cityName": "甘孜州",
- *             "currentConfirmedCount": 25,
- *             "confirmedCount": 78,
- *             "suspectedCount": 0,
- *             "curedCount": 53,
- *             "deadCount": 0,
- *             "locationId": 513300,
- *             "confirmedAddCount": 0,
- *             "suspectedAddCount": 0,
- *             "curedAddCount": 6,
- *             "deadAddCount": 0
- *         },
- *       ...
- *     ]
+ * {
+ *   "superiorPlace": "四川省",
+ *   "superiorLevel": "province",
+ *   "dataKind": "confirmedCount",
+ *   "inferiorPlaces": [
+ *   	{
+ *   		"place": "成都",
+ *   		"values": [1, 2, 10, 100, 120],
+ *   		"times": [1100, 1101, 1102, 1103]
+ *   	},
+ *   	{
+ *   		"place": "乐山",
+ *   		"values": [3, 5, 10, 100, 120],
+ *   		"times": [1100, 1101, 1102, 1105]
+ *   	},
+ *   	{
+ *   		"place": "眉山",
+ *   		"values": [3, 5, 10, 100, 120],
+ *   		"times": [1100, 1101, 1102, 1105]
+ *   	}
+ *   ]
+ * }
  */
-router.get('/epidemic/province', function (req, res, next) {
-    let provinceName = req.query.p;
-    if (provinceName === undefined) {
+router.get('/epidemic', function (req, res, next) {
+    let superiorPlace = req.query.superiorPlace;
+    let dataKind = req.query.dataKind;
+    if (superiorPlace === undefined || dataKind === undefined) {
         next(createError(400));
-        return
+        return;
     }
-    debug(`provinceName = ${provinceName}`);
-    let province = res.attached.area.find(val => val.provinceName === provinceName);
-    debug(`found province ${province}`);
-    if (province === undefined) {
-        res.status(404).send({cause: "Province not found."});
-        return
+    // todo unimplemented!
+    if (superiorPlace === "四川省" && dataKind === "confirmedCount") {
+        res.status(200).send(demoData);
+    } else {
+        res.status(404).end();
     }
-    res.status(200).send(province.cities);
-
 }, function (err, req, res, next) {
-    res.locals.message = `Wrong use of api.   Usage: ${req.path}?p=(province name)`;
+    res.locals.message = `Wrong use of api.   Usage: ${req.path}?superiorPlace={...}&dataKind={...}`;
     res.status(err.status).render('error', {error: err});
 });
 
