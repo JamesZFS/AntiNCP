09fa68cb2842162bd93f71349cd65d5a8cc976da
James Zheng <962907278@qq.com>
2020-03-24 01:13:51 +0800

    [fix] fetcher scheduler to function normally

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-screen -dm bash -c 'git pull origin $(git rev-parse --abbrev-ref HEAD); npm run env; npm run build; DEBUG=backend:* PORT=80 npm run start'
+screen -L -dm bash -c 'git pull origin dev; npm run env; npm run build; DEBUG=backend:* PORT=80 npm run start'
