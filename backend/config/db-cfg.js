017a10c1798f241a6ed2f0c52698a343700e1a83
郑逢时 <962907278@qq.com>
2020-03-23 14:56:33 +0800

    [add, update] database auto reconnect, fix some sequential logic #9

--- backend/config/db-cfg.js
+++ backend/config/db-cfg.js
@@ -6,15 +6,7 @@ const LOCAL_MYSQL_CFG = {
     multipleStatements: true,
 };
 
-const THUCG_MYSQL_CFG = {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'mdty2020',
-    port: '3336',
-    multipleStatements: true,
-};
-
-const TENCENT_MYSQL_CFG = {
+const TENCENT_MYSQL_CFG = { // only for testing purpose
     host: '129.204.207.38',
     user: 'root',
     password: 'mdty2020',
@@ -24,4 +16,4 @@ const TENCENT_MYSQL_CFG = {
 
 const EPIDEMIC_DATA_KINDS = ['confirmedCount', 'suspectedCount', 'curedCount', 'deadCount'];
 
-module.exports = {LOCAL_MYSQL_CFG, THUCG_MYSQL_CFG, TENCENT_MYSQL_CFG, EPIDEMIC_DATA_KINDS};
+module.exports = {LOCAL_MYSQL_CFG, TENCENT_MYSQL_CFG, EPIDEMIC_DATA_KINDS};
