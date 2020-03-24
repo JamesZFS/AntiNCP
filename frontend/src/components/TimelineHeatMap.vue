0023b7d15b0b711a3ee2da898084b90b60d4764c
王子轩 <zixuan-w17@mails.tsinghua.edu.cn>
2020-03-24 16:48:41 +0800

    [update]国内省级地图+时间轴实现 #2

--- frontend/src/components/TimelineHeatMap.vue
+++ frontend/src/components/TimelineHeatMap.vue
@@ -17,40 +17,40 @@
   import USA from '../assets/worldcountryjson/USA.json'
   // import China from '../assets/worldcountryjson/China.json'
   import world from 'echarts/map/js/world'
-  import 四川 from 'echarts/map/js/province/sichuan'
-  import 陕西 from 'echarts/map/js/province/shanxi1'
-  import 新疆 from 'echarts/map/js/province/xinjiang'
-  import 西藏 from 'echarts/map/js/province/xizang'
-  import 安徽 from 'echarts/map/js/province/anhui'
-  import 澳门 from 'echarts/map/js/province/aomen'
-  import 北京 from 'echarts/map/js/province/beijing'
-  import 重庆 from 'echarts/map/js/province/chongqing'
-  import 福建 from 'echarts/map/js/province/fujian'
-  import 甘肃 from 'echarts/map/js/province/gansu'
-  import 广东 from 'echarts/map/js/province/guangdong'
-  import 广西 from 'echarts/map/js/province/guangxi'
-  import 贵州 from 'echarts/map/js/province/guizhou'
-  import 海南 from 'echarts/map/js/province/hainan'
-  import 河北 from 'echarts/map/js/province/hebei'
-  import 黑龙江 from 'echarts/map/js/province/heilongjiang'
-  import 河南 from 'echarts/map/js/province/henan'
-  import 湖北 from 'echarts/map/js/province/hubei'
-  import 湖南 from 'echarts/map/js/province/hunan'
-  import 江苏 from 'echarts/map/js/province/jiangsu'
-  import 江西 from 'echarts/map/js/province/jiangxi'
-  import 吉林 from 'echarts/map/js/province/jilin'
-  import 辽宁 from 'echarts/map/js/province/liaoning'
-  import 内蒙古 from 'echarts/map/js/province/neimenggu'
-  import 宁夏 from 'echarts/map/js/province/ningxia'
-  import 青海 from 'echarts/map/js/province/qinghai'
-  import 山东 from 'echarts/map/js/province/shandong'
-  import 上海 from 'echarts/map/js/province/shanghai'
-  import 山西 from 'echarts/map/js/province/shanxi'
-  import 台湾 from 'echarts/map/js/province/taiwan'
-  import 天津 from 'echarts/map/js/province/tianjin'
-  import 香港 from 'echarts/map/js/province/xianggang'
-  import 云南 from 'echarts/map/js/province/yunnan'
-  import 浙江 from 'echarts/map/js/province/zhejiang'
+  import sichuan from 'echarts/map/json/province/sichuan'
+  import shanxi1 from 'echarts/map/json/province/shanxi1'
+  import xinjiang from 'echarts/map/json/province/xinjiang'
+  import xizang from 'echarts/map/json/province/xizang'
+  import anhui from 'echarts/map/json/province/anhui'
+  import aomen from 'echarts/map/json/province/aomen'
+  import beijing from 'echarts/map/json/province/beijing'
+  import chongqing from 'echarts/map/json/province/chongqing'
+  import fujian from 'echarts/map/json/province/fujian'
+  import gansu from 'echarts/map/json/province/gansu'
+  import guangdong from 'echarts/map/json/province/guangdong'
+  import guangxi from 'echarts/map/json/province/guangxi'
+  import guizhou from 'echarts/map/json/province/guizhou'
+  import hainan from 'echarts/map/json/province/hainan'
+  import hebei from 'echarts/map/json/province/hebei'
+  import heilongjiang from 'echarts/map/json/province/heilongjiang'
+  import henan from 'echarts/map/json/province/henan'
+  import hubei from 'echarts/map/json/province/hubei'
+  import hunan from 'echarts/map/json/province/hunan'
+  import jiangsu from 'echarts/map/json/province/jiangsu'
+  import jiangxi from 'echarts/map/json/province/jiangxi'
+  import jilin from 'echarts/map/json/province/jilin'
+  import liaoning from 'echarts/map/json/province/liaoning'
+  import neimenggu from 'echarts/map/json/province/neimenggu'
+  import ningxia from 'echarts/map/json/province/ningxia'
+  import qinghai from 'echarts/map/json/province/qinghai'
+  import shandong from 'echarts/map/json/province/shandong'
+  import shanghai from 'echarts/map/json/province/shanghai'
+  import shanxi from 'echarts/map/json/province/shanxi'
+  import taiwan from 'echarts/map/json/province/taiwan'
+  import tianjin from 'echarts/map/json/province/tianjin'
+  import xianggang from 'echarts/map/json/province/xianggang'
+  import yunnan from 'echarts/map/json/province/yunnan'
+  import zhejiang from 'echarts/map/json/province/zhejiang'
   import 'echarts/lib/component/visualMap'
   import vue from "vue";
 
@@ -67,22 +67,38 @@
     '甘肃省': '甘肃', '福建省': '福建', '西藏自治区': '西藏',
     '贵州省': '贵州', '辽宁省': '辽宁', '重庆市': '重庆',
     '陕西省': '陕西', '青海省': '青海', '香港特别行政区': '香港',
-    '黑龙江省': '黑龙江'
+    '黑龙江省': '黑龙江',
+    '那曲': '那曲地区'
   }
   //用于在发请求之前进行name转换
-  var name_filter_before_request = {
-    '北京市': '北京', '天津市': '天津', '上海市': '上海',
-    '云南省': '云南', '内蒙古自治区': '内蒙古', '台湾省': '台湾',
-    '吉林省': '吉林', '四川省': '四川', '宁夏回族自治区': '宁夏',
-    '安徽省': '安徽', '山东省': '山东', '山西省': '山西',
-    '广东省': '广东', '广西壮族自治区': '广西', '新疆维吾尔自治区': '新疆',
-    '江苏省': '江苏', '江西省': '江西', '河北省': '河北',
-    '河南省': '河南', '浙江省': '浙江', '海南省': '海南',
-    '湖北省': '湖北', '湖南省': '湖南', '澳门特别行政区': '澳门',
-    '甘肃省': '甘肃', '福建省': '福建', '西藏自治区': '西藏',
-    '贵州省': '贵州', '辽宁省': '辽宁', '重庆市': '重庆',
-    '陕西省': '陕西', '青海省': '青海', '香港特别行政区': '香港',
-    '黑龙江省': '黑龙江'
+  var request_filter = {
+    '北京': '北京市', '天津': '天津市', '上海': '上海市',
+    '云南': '云南省', '内蒙古': '内蒙古自治区', '台湾': '台湾省',
+    '吉林': '吉林省', '四川': '四川省', '宁夏': '宁夏回族自治区',
+    '安徽': '安徽省', '山东': '山东省', '山西': '山西省',
+    '广东': '广东省', '广西': '广西壮族自治区', '新疆': '新疆维吾尔自治区',
+    '江苏': '江苏省', '江西': '江西省', '河北': '河北省',
+    '河南': '河南省', '浙江': '浙江省', '海南': '海南省',
+    '湖北': '湖北省', '湖南': '湖南省', '澳门': '澳门特别行政区',
+    '甘肃': '甘肃省', '福建': '福建省', '西藏': '西藏自治区',
+    '贵州': '贵州省', '辽宁': '辽宁省', '重庆': '重庆市',
+    '陕西': '陕西省', '青海': '青海省', '香港': '香港特别行政区',
+    '黑龙江': '黑龙江省'
+  }
+  //用于用城市名找出对应的json文件
+  var nametojson = {
+    '北京': beijing, '天津': tianjin, '上海': shanghai,
+    '云南': yunnan, '内蒙古': neimenggu, '台湾': taiwan,
+    '吉林': jilin, '四川': sichuan, '宁夏': ningxia,
+    '安徽': anhui, '山东': shandong, '山西': shanxi,
+    '广东': guangdong, '广西': guangxi, '新疆': xinjiang,
+    '江苏': jiangsu, '江西': jiangxi, '河北': hebei,
+    '河南': henan, '浙江': zhejiang, '海南': hainan,
+    '湖北': hubei, '湖南': hunan, '澳门': aomen,
+    '甘肃': gansu, '福建': fujian, '西藏': xizang,
+    '贵州': guizhou, '辽宁': liaoning, '重庆': chongqing,
+    '陕西': shanxi1, '青海': qinghai, '香港': xianggang,
+    '黑龙江': heilongjiang
   }
 
   function dataFormatter(obj) {
@@ -252,6 +268,9 @@
         this.charts.resize()
       },
       dataImport(res) {
+        //清空this.myoption.options和时间标签
+        this.myoption.options.splice(0, this.myoption.options.length)
+        this.myoption.baseOption.timeline.data.splice(0,this.myoption.baseOption.timeline.data.length)
         //疑似、确诊、治愈、死亡数据依次导入，由于时间戳的个数相同，所以只需要一个循环
         //首先从疑似顺便导入时间戳，初始化option的个数
         for (var time_index in res.data.timeline['suspectedCount']) {
@@ -260,26 +279,22 @@
           tmp_suboption.title.text = time_index + this.cur_superiorPlace + '疫情状况'
           //疑似数据导入
           tmp_suboption.series[0].data = res.data.timeline['suspectedCount'][time_index]
-          for (var i = 0; i < tmp_suboption.series[0].data.length; i++) {
-            tmp_suboption.series[0].data[i].name = name_filter[tmp_suboption.series[0].data[i].name]
-          }
           //确诊数据导入
           tmp_suboption.series[1].data = res.data.timeline['confirmedCount'][time_index]
-          for (var i = 0; i < tmp_suboption.series[1].data.length; i++) {
-            tmp_suboption.series[1].data[i].name = name_filter[tmp_suboption.series[1].data[i].name]
-          }
           //治愈数据导入
           tmp_suboption.series[2].data = res.data.timeline['curedCount'][time_index]
-          for (var i = 0; i < tmp_suboption.series[2].data.length; i++) {
-            tmp_suboption.series[2].data[i].name = name_filter[tmp_suboption.series[2].data[i].name]
-          }
           //死亡数据导入
           tmp_suboption.series[3].data = res.data.timeline['deadCount'][time_index]
+          //name修正
           for (var i = 0; i < tmp_suboption.series[3].data.length; i++) {
-            tmp_suboption.series[3].data[i].name = name_filter[tmp_suboption.series[3].data[i].name]
+            if(name_filter[tmp_suboption.series[0].data[i].name]!= undefined)
+            {
+              tmp_suboption.series[0].data[i].name = name_filter[tmp_suboption.series[0].data[i].name]
+              tmp_suboption.series[1].data[i].name = name_filter[tmp_suboption.series[1].data[i].name]
+              tmp_suboption.series[2].data[i].name = name_filter[tmp_suboption.series[2].data[i].name]
+              tmp_suboption.series[3].data[i].name = name_filter[tmp_suboption.series[3].data[i].name]
+            }
           }
-          //清空this.myoption.options
-          this.myoption.options.clear()
           this.myoption.options.push(tmp_suboption)
         }
       },
@@ -304,22 +319,37 @@
           } catch (err) {
             vue.$log.error(`backend communication test failed with ${err}`);
           }
-        } else if (this.cur_superiorLevel == 'world') {
-
+        } else if (this.cur_superiorLevel == 'province') {
+          try {
+            let res = await vue.axios.get('/api/retrieve/epidemic/timeline/province', {
+              params: {
+                dataKind: 'suspectedCount,confirmedCount,curedCount,deadCount',
+                country: '中国',
+                province: request_filter[this.cur_superiorPlace],
+                verbose: ''
+              }
+            })
+            this.dataImport(res)
+            this.drawTimeAxis()
+            console.log(res)
+          } catch (err) {
+            vue.$log.error(`backend communication test failed with ${err}`);
+          }
+        }
+        else if(this.cur_superiorLevel == 'world'){
+          console.log('unfinished')
         }
 
       },
       returnworldmap() {
         this.cur_superiorPlace = 'world'
         this.cur_superiorLevel = 'world'
-        this.charts.setOption(this.myoption, true)
-        this.charts.resize()
+        this.get_epidemic_data()
       },
       returnchinamap() {
         this.cur_superiorPlace = 'china'
         this.cur_superiorLevel = 'country'
-        this.charts.setOption(this.myoption, true)
-        this.charts.resize()
+        this.get_epidemic_data()
         // console.log(this.myoption)
       }
     },
@@ -333,6 +363,7 @@
       })
       this.charts.on('click', (params) => {
         //如果点击事件发生在时间轴上
+        // console.log(params)
         if (params.componentType == 'timeline') {
           this.myoption.baseOption.timeline.currentIndex = params.dataIndex
           this.charts.setOption(this.myoption, true)
@@ -352,6 +383,8 @@
           } else if (this.cur_superiorLevel == 'country' && this.cur_superiorPlace == 'china') {
             this.cur_superiorLevel = 'province'
             this.cur_superiorPlace = params.name
+            echarts.registerMap(params.name, nametojson[params.name])
+            console.log(params.name)
             this.get_epidemic_data()
           }
         }
@@ -359,6 +392,7 @@
       })
       var tmp_maxdic = this.maxdic
       this.charts.on('legendselectchanged', (obj) => {
+        console.log(obj)
         var tmp_max = tmp_maxdic[obj.name]
         for (var i = 0; i < this.myoption.options.length; i++) {
           this.myoption.options[i].visualMap.max = tmp_max
