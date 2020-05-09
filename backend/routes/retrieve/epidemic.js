a534880c10a20665a133a41add9624323935f8b6
郑逢时 <962907278@qq.com>
2020-05-09 20:11:43 +0800

    [fix] date mess up problem in PredictionMap

--- backend/routes/retrieve/epidemic.js
+++ backend/routes/retrieve/epidemic.js
@@ -98,11 +98,11 @@ router.get('/timeline/world', async function (req, res) {
             for (let dataKind of dataKinds) {
                 worldTimeline.datasets[dataKind] = [];
             }
-            let fields = [`DATE_FORMAT(date,'%m-%d') AS date`].concat(dataKinds.map(val => `SUM(${val}) AS ${val}`));
+            let fields = [`DATE_FORMAT(date,'%m-%d') AS label`].concat(dataKinds.map(val => `SUM(${val}) AS ${val}`));
             let condition = `province='' AND date BETWEEN ${dateMin} AND ${dateMax}`; // for where clause
             let result = await db.selectEpidemicData(fields, condition, false, 'GROUP BY date ORDER BY date ASC');
             for (let item of result) {
-                worldTimeline.labels.push(item.date);
+                worldTimeline.labels.push(item.label);
                 for (let dataKind of dataKinds) {
                     worldTimeline.datasets[dataKind].push(item[dataKind]);
                 }
@@ -249,11 +249,11 @@ router.get('/timeline/country', async function (req, res) {
             for (let dataKind of dataKinds) {
                 countryTimeline.datasets[dataKind] = [];
             }
-            let fields = [`DATE_FORMAT(date,'%m-%d') AS date`].concat(dataKinds);
+            let fields = [`DATE_FORMAT(date,'%m-%d') AS label`].concat(dataKinds);
             let condition = `country=${db.escape(country)} AND province='' AND date BETWEEN ${dateMin} AND ${dateMax}`; // for where clause
             let result = await db.selectEpidemicData(fields, condition, false, 'GROUP BY date ORDER BY date ASC');
             for (let item of result) {
-                countryTimeline.labels.push(item.date);
+                countryTimeline.labels.push(item.label);
                 for (let dataKind of dataKinds) {
                     countryTimeline.datasets[dataKind].push(item[dataKind]);
                 }
@@ -431,11 +431,11 @@ router.get('/timeline/province', async function (req, res) {
             for (let dataKind of dataKinds) {
                 provinceTimeline.datasets[dataKind] = [];
             }
-            let fields = [`DATE_FORMAT(date,'%m-%d') AS date`].concat(dataKinds);
+            let fields = [`DATE_FORMAT(date,'%m-%d') AS label`].concat(dataKinds);
             let condition = `country=${db.escape(country)} AND province=${db.escape(province)} AND city='' AND date BETWEEN ${dateMin} AND ${dateMax}`; // for where clause
             let result = await db.selectEpidemicData(fields, condition, false, 'GROUP BY date ORDER BY date ASC');
             for (let item of result) {
-                provinceTimeline.labels.push(item.date);
+                provinceTimeline.labels.push(item.label);
                 for (let dataKind of dataKinds) {
                     provinceTimeline.datasets[dataKind].push(item[dataKind]);
                 }
